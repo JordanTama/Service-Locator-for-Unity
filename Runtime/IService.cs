b0a0cf8685c297bc97acf4ecea6add7fbe8fcdb2
Jordan Hamlin <Jordan.T.Hamlin@gmail.com>
2022-10-25 16:25:04 +1100

    Initial commit.

---
+++ Runtime/IService.cs
@@ -0,0 +1,5 @@
+public interface IService
+{
+    void OnRegistered();
+    void OnUnregistered();
+}
---
+++ Runtime/Locator.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Locator
+{
+    private static readonly Dictionary<string, IService> Services = new();
+
+    public static void Register<T>(T service) where T : IService
+    {
+        if (typeof(T) == typeof(IService))
+        {
+            Debug.LogError("Tried to register service of type IService. Register service as its actual type, rather than an IService.");
+            return;
+        }
+        
+        string key = GetServiceKey<T>();
+        if (Services.ContainsKey(key))
+        {
+            Debug.LogError($"Service of type {typeof(T).Name} already registered!");
+            return;
+        }
+
+        Services.Add(key, service);
+        service.OnRegistered();
+    }
+
+    public static void Unregister<T>(T service) where T : IService
+    {
+        string key = GetServiceKey<T>();
+        if (!Services.ContainsKey(key))
+        {
+            Debug.LogError($"Service of type {typeof(T).Name} not registered!");
+            return;
+        }
+
+        Services.Remove(key);
+        service.OnUnregistered();
+    }
+
+    public static void Get<T>(out T service) where T : IService
+    {
+        string key = GetServiceKey<T>();
+        if (!Services.ContainsKey(key))
+        {
+            service = default;
+            return;
+        }
+
+        service = (T) Services[key];
+    }
+
+    public static bool IsRegistered<T>() where T : IService => Services.ContainsKey(GetServiceKey<T>());
+
+    private static string GetServiceKey<T>() where T : IService => typeof(T).FullName;
+}
